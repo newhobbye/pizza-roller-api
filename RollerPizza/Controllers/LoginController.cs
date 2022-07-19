ceb2e1b7d38168c7bac4f6ad2438668488b75223
Robson Monteiro <rmonteiro@brasilcash.com.br>
2022-07-19 15:39:48 -0300

    Implementação de login e autenticação

---
+++ RollerPizza/Controllers/LoginController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Security.Claims;
+
+namespace RollerPizza.Controllers
+{
+    [ApiController]
+    [Route("api/pizza/login")]
+    public class LoginController : Controller
+    {
+        IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("userAndPassword")]
+        public async Task<IActionResult> LoginRollerPizzaClient(string nickName, string password)
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { Mensagem = "Usuário já está logado!" });
+            }
+
+            MySqlConnection mySqlConnection = new("server = localhost; database = PizzaRoller; user = Robson; password = R55108105");
+            await mySqlConnection.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT * FROM client  WHERE NickName = '{nickName}' AND Password = '{password}'";
+
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+                if(await reader.ReadAsync())
+                {
+                    string userCPFId = reader.GetString(0);
+                    string name = reader.GetString(1);
+                    string email = reader.GetString(4);
+
+                    List<Claim> securityAccess = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, userCPFId),
+                        new Claim(ClaimTypes.Name, name),
+                        new Claim(ClaimTypes.Email, email),
+                    };
+
+                    var identity = new ClaimsIdentity(securityAccess, "Identity.Login");
+                    var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                    await HttpContext.SignInAsync(userPrincipal,
+                        new AuthenticationProperties
+                        {
+                            IsPersistent = false,
+                            ExpiresUtc = DateTime.UtcNow.AddDays(1),
+                        }
+                        );
+
+                    return Json(new { Mensagem = "Usuário logado com sucesso!" });
+                }
+            
+            return Json(new { Mensagem = "Usuário não encontrado! Verifique a o nick ou a senha!" });
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+
+            return RedirectToAction("LoginRollerPizzaClient", "Login");
+        }
+
+    }
+
+
+    
+}
--- RollerPizza/Program.cs
+++ RollerPizza/Program.cs
@@ -4,7 +4,7 @@ using RollerPizza.Data.Dao;
 using RollerPizza.Model;
 using RollerPizza.Service;
 using RollerPizza.Service.Use_Case;
-
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("ContextConnection");
@@ -22,6 +22,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
 
 #region"Dependencia do contexto"
 builder.Services.AddScoped<DBContext>();
@@ -47,6 +52,17 @@ builder.Services.AddScoped<PaymentHandler>();
 builder.Services.AddScoped<ShoppingKartService>();
 #endregion
 
+#region"Dependencia de Autenticação de login"
+builder.Services.AddAuthentication("Identity.Login")
+    .AddCookie("Identity.Login", config =>
+    {
+        config.Cookie.Name = "Identity.Login";
+        config.LoginPath = "/api/pizza/login";
+        config.AccessDeniedPath = "/api/pizza/login";
+        config.ExpireTimeSpan = TimeSpan.FromDays(1);
+    });
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -58,6 +74,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+#region"Definir para usar autenticação"
+app.UseAuthentication();
+#endregion
 
 app.UseAuthorization();
 
